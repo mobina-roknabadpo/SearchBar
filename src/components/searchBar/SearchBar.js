2c369e42a723fdb6aa4ecf8acc8d239f87a58fb4
Mobina Roknabadpo <mobina.rp1380@gmail.com>
2023-09-26 13:34:12 +0330

    feat: created search function

--- src/components/searchBar/SearchBar.js
+++ src/components/searchBar/SearchBar.js
@@ -1,9 +1,18 @@
-import React from 'react'
-
-const SearchBar = () => {
+import React from "react";
+import styles from "./searchBar.module.css";
+const SearchBar = ({ text, setText }) => {
   return (
-    <div>SearchBar</div>
-  )
-}
+    <div className={styles.container}>
+      <img src="./assets/images/SearchIcon.png" className={styles.searchIcon} />
+      <input
+        type="text"
+        value={text}
+        placeholder="فیلم، سریال، بازیگر و ژانر"
+        className={styles.formInput}
+        onChange={(e) => setText(e.target.value)}
+      />
+    </div>
+  );
+};
 
-export default SearchBar
+export default SearchBar;
