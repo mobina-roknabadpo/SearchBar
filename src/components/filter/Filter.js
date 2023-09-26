35f5da854d4037d2c7355f4dceb6d31785fc202e
Mobina Roknabadpo <mobina.rp1380@gmail.com>
2023-09-26 13:33:40 +0330

    feat: created filter function

--- src/components/filter/Filter.js
+++ src/components/filter/Filter.js
@@ -1,9 +1,24 @@
-import React from 'react'
+import React from "react";
+import styles from "./filter.module.css";
 
 const Filter = () => {
   return (
-    <div>Filter</div>
-  )
-}
+    <div className={styles.container}>
+      <div className={styles['filter-container']}>
+        <h2 className={styles.name}>فیلترها</h2>
+        <div className={styles.option}>
+          <div className={styles.movie}>
+            <input type="checkbox" id="movie" />
+            <label htmlFor="movie">فیلم</label>
+          </div>
+          <div className={styles.serial}>
+            <input type="checkbox" id="serial" />
+            <label htmlFor="serial">سریال </label>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
 
-export default Filter
+export default Filter;
