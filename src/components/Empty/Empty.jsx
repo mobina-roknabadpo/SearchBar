c15fc42dbdc7ef4923e6f5a74a20d99ff5b7a24a
Mobina Roknabadpo <mobina.rp1380@gmail.com>
2023-09-26 13:38:14 +0330

    feat: created empty component

---
+++ src/components/Empty/Empty.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import EmptySVG from "../SVGs/EmptySVG";
+import styles from "./empty.module.css";
+
+function Empty() {
+  return (
+    <div className={styles.container}>
+      <EmptySVG />
+      <p className={styles.paragraph}>
+        عنوان فیلم، سریال یا بازیگر مورد نظر خود را جستجو کنید و یا از طریق
+        فیلتر‌های موجود، فیلم و سریال مورد علاقه خود را پیدا کنید.
+      </p>
+    </div>
+  );
+}
+
+export default Empty;
