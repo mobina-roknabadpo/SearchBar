4ae6b64d299b5171a68509db1f9b93ba05781487
Mobina Roknabadpo <mobina.rp1380@gmail.com>
2023-09-26 13:33:13 +0330

    feat: created header function

--- src/components/header/Haeder.js
+++ src/components/header/Haeder.js
@@ -1,11 +1,55 @@
-import React from 'react'
+import React from "react";
+import header from './header.module.css'
 
-const Header = ()=>{
-    return (
-        <div>
-            Header
-        </div>
-    )
-}
+const Header = () => {
+  return (
+    <>
+      <header>
+        <a href="https://www.namava.ir">
+          <img src="./assets/images/Logo.png" alt="namava" className={header.logo} />
+        </a>
+        <nav>
+          <ul className={header.menu}>
+            <li className={header.item}>
+              <a>خانه</a>
+            </li>
+            <li className={header.item}>
+              <a>فیلم ها</a>
+            </li>
+            <li className={header.item}>
+              <a>سریال ها</a>
+            </li>
+            <li className={header.item}>
+              <a>دسته بندی</a>
+            </li>
+            <li className={header.item}>
+              <a>تازه ها</a>
+            </li>
+            <li className={header.item}>
+              <a>کودکان</a>
+            </li>
+            <li className={header.item}>
+              <a>پردیس نماوا</a>
+            </li>
+            <li className={header.item}>
+              <a>نماوا مگ</a>
+            </li>
+          </ul>
+          <div className={header.info}>
+            <div>
+              <img src="./assets/images/Search.png" alt="search" className={header.search}/>
+            </div>
+            <div>
+              <img src="./assets/images/Message.png" alt="message" className={header.message}/>
+            </div>
+            <div>
+              <img src="./assets/images/profile.png" alt="profile" className={header.profile}/>
+            </div>
+          </div>
+        </nav>
+      </header>
+    </>
+  );
+};
 
-export default Header
+export default Header;
