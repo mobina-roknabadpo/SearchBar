2ef8e97b3cec8d60b6b384dec727b93c5a97a01d
Mobina Roknabadpo <mobina.rp1380@gmail.com>
2023-09-26 13:36:39 +0330

    feat: use components

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
-import React from "react";
-
+import React, { useState } from "react";
+import Layout from "./components/Layout/Layout";
+import SearchBar from "./components/SearchBar/SearchBar";
 const App = () => {
+  const [text, setText] = useState("");
+  const [cards, setCards] = useState([]);
+  console.log("text", text)
   return (
-    <>
-      <div>App</div>
-    </>
+    <Layout>
+      <SearchBar text={text} setText={setText} />
+    </Layout>
   );
 };
 
