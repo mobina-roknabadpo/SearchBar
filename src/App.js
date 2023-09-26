bbac5e4fa75736c46bb9922934d4e573081ade6a
Mobina Roknabadpo <mobina.rp1380@gmail.com>
2023-09-26 14:26:27 +0330

    feat: used component in app

--- src/App.js
+++ src/App.js
@@ -1,13 +1,17 @@
 import React, { useState } from "react";
 import Layout from "./components/Layout/Layout.jsx";
 import SearchBar from "./components/SearchBar/SearchBar.jsx";
+// import Empty from "./components/Empty/Empty.jsx";
+import Undefined from './components/Undefined/Undefined.jsx'
 const App = () => {
   const [text, setText] = useState("");
   const [cards, setCards] = useState([]);
-  console.log("text", text)
+  console.log("text", text);
   return (
     <Layout>
       <SearchBar text={text} setText={setText} />
+      {/* <Empty /> */}
+      <Undefined/>
     </Layout>
   );
 };
