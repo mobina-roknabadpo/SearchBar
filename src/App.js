f6bf3b3541e989e92b1b231990e23dd8687c9eeb
Mobina Roknabadpo <mobina.rp1380@gmail.com>
2023-09-30 13:41:22 +0330

    feat: get response

--- src/App.js
+++ src/App.js
@@ -1,17 +1,42 @@
 import React, { useState } from "react";
 import Layout from "./components/Layout/Layout.jsx";
+import "./App.css";
 import SearchBar from "./components/SearchBar/SearchBar.jsx";
-// import Empty from "./components/Empty/Empty.jsx";
-import Undefined from './components/Undefined/Undefined.jsx'
+import { useEffect } from "react";
+import axios from "axios";
+import Filter from "./components/Filter/Filter.jsx";
 const App = () => {
-  const [text, setText] = useState("");
+  const [search, setSearch] = useState("");
   const [cards, setCards] = useState([]);
-  console.log("text", text);
+  const [type, setType] = useState("all");
+  const [page, setPage] = useState(1);
+
+  useEffect(() => {
+    if (search.length > 0) {
+      getMovies();
+    }
+  }, [search]);
+
+  console.log(search);
+  async function getMovies() {
+    await axios
+      .get(
+        `https://www.namava.ir/api/v3.0/search/advance?type=${type}&count=20&page=${page}&query=${search}`
+      )
+      .then((response) => {
+        const result = response.data.result.result_items[0].groups.Media.items;
+        console.log(result);
+        setCards(result);
+      })
+      .catch((err) => console.log(err));
+  }
+
   return (
-    <Layout>
-      <SearchBar text={text} setText={setText} />
-      {/* <Empty /> */}
-      <Undefined/>
+    <Layout cards={cards} search={search}>
+      <div className="container">
+        <Filter />
+        <SearchBar search={search} setSearch={setSearch} cards={cards} />
+      </div>
     </Layout>
   );
 };
