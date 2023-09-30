c30a7c76233492b98a13e104b095bc61b7bbcd26
Mobina Roknabadpo <mobina.rp1380@gmail.com>
2023-09-30 13:39:15 +0330

    change: changed content

--- src/components/Layout/Layout.jsx
+++ src/components/Layout/Layout.jsx
@@ -2,16 +2,17 @@ import React from "react";
 import Header from "../Header/Haeder.jsx";
 import layout from "./layout.module.css";
 import Filter from "../Filter/Filter.jsx";
-function Layout({ children }) {
+import ListCards from "../ListCards/ListCards.jsx";
+import Empty from "../Empty/Empty.jsx";
+import Undefined from "../Undefined/Undefined.jsx";
+function Layout({ children, cards, search }) {
   return (
     <>
       <Header />
-      <div className={layout.container}>
-        <Filter />
-        <div className={layout.flex}>{children}</div>
-      </div>
+      <div className={layout.container}>{children}</div>
     </>
   );
+  
 }
 
 export default Layout;
