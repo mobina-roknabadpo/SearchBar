7d969d68cd723d90af4cf395c5eedc537089ffa1
Mobina Roknabadpo <mobina.rp1380@gmail.com>
2023-09-30 13:38:38 +0330

    change: move search component to another component

--- src/components/searchBar/SearchBar.jsx
+++ src/components/searchBar/SearchBar.jsx
@@ -1,18 +1,17 @@
 import React from "react";
 import styles from "./searchBar.module.css";
-const SearchBar = ({ text, setText }) => {
+import FormInput from "../FormInput/FormInput.jsx";
+import ListCards from "../ListCards/ListCards.jsx";
+import Undefined from "../Undefined/Undefined.jsx";
+import Empty from "../Empty/Empty.jsx";
+function SearchBar({ search, setSearch, cards }) {
   return (
     <div className={styles.container}>
-      <img src="./assets/images/SearchIcon.png" className={styles.searchIcon} />
-      <input
-        type="text"
-        value={text}
-        placeholder="فیلم، سریال، بازیگر و ژانر"
-        className={styles.formInput}
-        onChange={(e) => setText(e.target.value)}
-      />
+      <FormInput search={search} setSearch={setSearch} />
+      {!!search.length && !cards.length && <Undefined />}
+      {search === "" ? <Empty /> : <ListCards cards={cards} />}
     </div>
   );
-};
+}
 
 export default SearchBar;
