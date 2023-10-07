2f1155b66f07f63953750fd34a0526f8b5f6687d
Mobina Roknabadpo <mobina.rp1380@gmail.com>
2023-10-07 17:33:01 +0330

    feat: added loading component

---
+++ src/components/Loading/Loading.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import styles from "./style.module.css";
+
+const Loading = () => {
+  return (
+    // <div className={styles.loaderContainer}>
+    //   <div className={styles.spinner}></div>
+    // </div>
+    <div className={styles.loaderContainer}>is Loading...</div>
+  );
+};
+
+export default Loading;
